8f34e4c3271b042f284fb61a7fa5da9265682256
Bikash Pattnayak <bikash.pattnayak@mediakind.com>
2020-12-10 23:01:53 +0530

    changed the port

--- controller/home.js
+++ controller/home.js
@@ -1,6 +1,6 @@
 class HomeController {
   healthCheck = async (req, res, next) => {
-    return res.status(200).json("Alz is well");
+    return res.status(200).json({ status: 0, message: "Alz is well" });
   };
 }
 
